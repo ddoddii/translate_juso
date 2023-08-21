d3162c589b34939fda6365922334f2aaee974394
Soeun Uhm <uhmturks@yonsei.ac.kr>
2023-08-21 22:11:22 +0900

    fix device

--- api.py
+++ api.py
@@ -4,65 +4,10 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel, Field
 import uvicorn
 from juso_api_kor import find_juso_kor
+import torch
 # from main import fin_juso_model
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
-from re_check import extract_address_info
-import re
-
-#device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model_ckpt = './model'
-model = AutoModelForSeq2SeqLM.from_pretrained(model_ckpt)
-tokenizer = AutoTokenizer.from_pretrained(model_ckpt)
-
-def juso_model(input_text):    
-    text_cleaned = re.sub(r'[^\w\s()%-]', '', input_text)
-
-    inputs = tokenizer(text_cleaned, return_tensors="pt", max_length=64, padding='longest')
-    koreans = model.generate(
-        **inputs,
-        max_length=64,
-        num_beams=2,
-    )
-
-    preds = tokenizer.batch_decode( koreans, skip_special_tokens=True )
-    output = preds[0]
-    
-    return output
-
-def check_juso(ori_input, model_output):
-    re_road_name,  re_city_name = extract_address_info(ori_input)
-    #md_building_number, md_road_name, md_gil_name , md_city_name, md_si_name, md_province_name = extract_address_info(model_output)
-    yes_or_no = False
-    if re.findall(r'\d', ori_input):
-        pass
-    else:
-        result = ''
-
-    if re_road_name :
-        result = model_output
-    else:
-        yes_or_no = True
-        result = re.sub(r'(\S+(?:로|대로|길)\b)','',model_output)
-    
-    if re_city_name :
-        pass
-    else:
-        yes_or_no = True
-        result = re.sub(r'\s*(\S+(?:구|군)\b)| \S+(?:시)\b','',result)
-        
-    if re_road_name and re_city_name:
-        parts = model_output.split()
-        new_parts = parts[1:]
-        result = ' '.join(new_parts)
-
-    return result, yes_or_no
-
-def fin_juso_model(input):
-    model_output = juso_model(input)
-    ori_input = input
-    checked_juso, yes_or_no = check_juso(ori_input, model_output)
-    
-    return checked_juso, yes_or_no
+from main import fin_juso_model
 
 
 app = FastAPI(
@@ -113,9 +58,6 @@ async def process_request(request: ClientRequest):
         juso_model_output, yes_or_no = fin_juso_model(request_address)
         real_juso, num = find_juso_kor(juso_model_output)
         if not real_juso:  
-            # Return the FailureResponse
-            """ failure_header = Header(RESULT_CODE=ResultCode.FAILURE, RESULT_MSG=f"seq {individual_request.seq} is failed to transfer")
-            raise HTTPException(status_code=400, detail={"HEADER": failure_header.dict()}) """
             real_juso = '답 없음'
             body_data_list.append(BodyData(seq=individual_request.seq, resultAddress=real_juso))
         elif yes_or_no and num>1:
